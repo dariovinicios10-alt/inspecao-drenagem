60bb0e896c61afbf795b2950dcbec9f652ebd738
Dario Vinicius <dariovinicios10@gmail.com>
2026-07-18 06:59:50 -0400

    Remove 344 drenagens ja inspecionadas em 17/07 + bump SW v25

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * ===================================================================== */
 'use strict';
 
-const VERSAO_CACHE = 'drenagem-v24';
+const VERSAO_CACHE = 'drenagem-v25';
 
 // Arquivos essenciais do app (mesma pasta no SharePoint)
 const ARQUIVOS_APP = [
